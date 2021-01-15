972dea455582ae5128094ff9a18548582888d700
mshahbazali <shahbazattari121@gmail.com>
2021-01-15 02:42:31 -0800

    JSClass2

---
+++ app.js
@@ -0,0 +1,104 @@
+// Q1
+// var name = prompt("Enter Your Name");
+// document.write(name.toUpperCase());
+
+// Q2
+// var name = prompt("Enter Your Name");
+// document.write(name.toLowerCase());
+
+// Q3
+// var mobileModel = prompt("Enter Your Favorite Mobile Model");
+// var lengthMobile = mobileModel.length;
+// if (lengthMobile > 3){
+//     document.write(mobileModel.slice(0))
+// }
+
+// Q4
+// var str = prompt("Enter Your Father Name");
+// document.write(str.charAt(str.length - 1));
+
+// Q5
+// var str = "Pakistani"
+// console.log(str.indexOf("n"));
+
+// Q6
+// var userName = prompt("Enter Your UserName");
+// for(var i = 0; i < userName.length; i++){
+// if(userName.charAt(i) === "@"){
+//     alert("enter a valid username.")
+//     break;
+// }
+// }
+
+// Q7
+// var str = "The quick brown fox jumps over the lazy dog";
+// findStr = str.indexOf("the")
+// console.log(findStr);
+
+// Q8
+// var str = "Pakistani";
+// document.write(str.indexOf("i"));
+
+// Q9
+// var str = "Hyderabad";
+// var replaceStr = str.replace("Hyderabad", "Hyder Islam");
+// document.write(replaceStr);
+
+// Q10
+// var str = "“Ali and Sami are best friends. They play cricket and football together"
+// var replaceStr = str.replace("and","&");
+// document.write(replaceStr);
+
+// Q11
+// var  num = prompt("Enter Your Value");
+// rounded = Math.round(num);
+// document.write(rounded)
+
+// Q12
+// var  num = prompt("Enter Your Value");
+// rounded = Math.round(-num);
+// document.write(rounded)
+
+
+// Q13
+// var num = Math.random()
+// document.write(num)
+
+// Q14
+
+// var head = Math.random(head);
+// var tail = Math.random(tail);
+// document.write("Head" + " " + (head * 2 ) + "<br />" + "Tail" + " " + (tail * 3))
+
+// Q15
+
+// var num = prompt("Enter Your Value 1 to 10");
+// var secretNum = "4"
+// if(num === secretNum){
+
+//     alert("Congratulate")
+// }
+// else(
+
+//     alert("Wrong again try")
+// )
+
+// Q16
+
+// var weight = prompt("Enter Your Weight");
+// document.write(parseInt(weight))
+
+// Q17
+// var str = "7325";
+// console.log(Number(str));
+
+// Q18
+// var num = 72.64.toString();
+// var str = num.replace(".", "");
+// document.write(str);
+
+// Q19
+// var parcentage = 30/45 * 100;
+// document.write(parcentage.toFixed(2))
+
+
